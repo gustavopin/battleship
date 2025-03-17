08f7da42037ab4ab7d702bcd610564d535aae857
gustavopin <gustavorp96@gmail.com>
2025-03-17 13:40:00 -0300

    Added more functions to the code, as well a switch function and the abilities menu and functionality. Updated the README.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## Introduction
 
-This program will represent a game of battleship using arrays, matrixes and other functions in C.
+This program will represent a game of battleship using arrays, matrixes and conditionals.
+
+### Technical Information
+
+The porogram prints a matrix 10x10 and indexes for columns (letters A through J) and lines (1 through 10) and also 4 ships, 2 in diagonal, 1 in vertical and 1 in horizontal.
+
+The ships are represented by the number 3, while water is represented by the number 0.
+
+There is a swtich function to add some complexity and choice to the game that will make the player choose between 3 abilities:
+
+ - Cone: represented by the number 9 it will print a cone shape within the battlezone.
+ - Cross: represented by the number 8, it will print a cross shape within the battlezone.
+ - Octahedron: represented by the number 7, it will print a cross shape within the battlezone.
+
+All the ships and abilities are pre-determined in size and position by the program.
--- battleship.c
+++ battleship.c
@@ -1,10 +1,15 @@
 #include <stdio.h> 
 
+//defining some fixed arrays to easy the maintenence
+#define LINES 10
+#define COLUMNS 10
+
 int main(){
 
     char* col_id[10] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
     int line_id[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int battle_zone[10][10];
+    int battle_zone[LINES][COLUMNS];
+    int ability;
 
     printf("Battle Zone\n");
     printf("  ");
@@ -12,51 +17,124 @@ int main(){
     //this will print letters A through J to represent the values for the columns of our battle zone
     for (int i = 0; i < 10; i++)
     {
-        printf("%s ", col_id[i]);
+        printf("%s ", col_id[i]); //print the index for the colums (A through J)
     }
     //skipping a line for the battle zone
     printf("\n");
     
     //creating the zone, each house will have the value 0
-    for (size_t i = 0; i < 10; i++)
+    //we are also creating some ships in line 2 (columns 3,4,5) and column 6 (lines 5,6,7)
+    //also on the diagonals:[(1, 1)(2, 2)(3, 3)] [(5, 3)(6, 4)(7, 5)]
+    for (size_t i = 0; i < LINES; i++)
     {
-        for (int j = 0; j < 10; j++)
+        printf("%d ", line_id[i]); //print the index for the lines (1 thourgh 10)
+        
+        for (int j = 0; j < COLUMNS; j++)
         {
-            battle_zone[i][j] = 0;
-        }
-    }
-    
-    //adding 2 ships
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            if ((i == 2 && j == 3) || (i== 2 && j == 4) || (i == 2 && j == 5))
+            if ((i == 1 && j == 1)||(i == 2 && j == 2) || (i == 3 && j == 3)) //ship1 (diagonal)
+            {
+                battle_zone[i][j] = 3;   
+            } else if ((i == 2 && j == 4) || (i== 2 && j == 5) || (i == 2 && j == 6)) //ship2 (line) 
             {
                 battle_zone[i][j] = 3;
+            } else if ((i == 5 && j == 8) || (i== 6 && j == 8) || (i == 7 && j == 8)) //ship3 (vertical line)
+            {
+                battle_zone[i][j] = 3;
+            } else if ((i == 5 && j == 3)||(i == 6 && j == 4) || (i == 7 && j == 5)) //ship 4 (diagonal)
+            {
+                battle_zone[i][j] = 3;
+            } else {
+                battle_zone[i][j] = 0; //creating the water
             }
+            printf("%d ", battle_zone[i][j]); //printing the battlezone and the ships following the conditions
+        }
+        printf("\n"); //skiping a line
+    }
 
-            if ((i == 5 && j == 6) || (i== 6 && j == 6) || (i == 7 && j == 6))
-            {
-                battle_zone[i][j] = 3;
-            }  
-        }
-    }
-    
-    //printing the battle zone
-    for (size_t i = 0; i < 10; i++)
+    //creating abilities
+    printf("Choose the ability you want to use:\n");
+    printf("1 - Cone\n");
+    printf("2 - Cross\n");
+    printf("3 - Octahedron\n");
+    printf("Ability number: ");
+    scanf("%d", &ability);
+
+    switch (ability)
     {
-        for (int j = 0; j < 10; j++)
+    case 1:
+
+        for (int i = 0; i < 10; i++)
         {
-            if (j == 0)
-            {
-                printf("%d ", line_id[i]);
-            }
-            
-            printf("%d ", battle_zone[i][j]);
+            printf("%s ", col_id[i]); //print the index for the colums (A through J)
         }
-        printf("\n");
+        printf("\n"); //skip a line after the index
+
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%d ", line_id[i]); //print the index for the lines (1 thourgh 10)
+
+            for (int j = 0; j < 10; j++)
+            {
+                if ((i == 4 && j == 4) || (i == 5 && (j == 3 || j == 4 || j == 5)) || (i == 6 && (j == 2 || j == 3 || j == 4 || j == 5 || j == 6))) 
+                {
+                    battle_zone[i][j] = 9;
+                }
+                printf("%d ", battle_zone[i][j]);
+            }
+            printf("\n"); 
+        }
+        break;
+
+    case 2:
+
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%s ", col_id[i]); //print the index for the colums (A through J)
+        }
+        printf("\n"); //skip a line after the index
+
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%d ", line_id[i]); //print the index for the lines (1 thourgh 10)
+
+            for (int j = 0; j < 10; j++)
+            {
+                if ((i == 6 && j == 3) || (i == 7 && (j == 1 || j == 2 || j == 3 || j == 4 || j == 5)) || (i == 8 && j == 3)) 
+                {
+                    battle_zone[i][j] = 8;
+                }
+                printf("%d ", battle_zone[i][j]);
+            }
+            printf("\n"); 
+        }
+        break;
+
+    case 3:
+
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%s ", col_id[i]); //print the index for the colums (A through J)
+        }
+        printf("\n"); //skip a line after the index
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%d ", line_id[i]); //print the index for the lines (1 thourgh 10)
+
+            for (int j = 0; j < 10; j++)
+            {
+                if ((i == 0 && j == 5) || (i == 1 && (j == 4 || j == 5 || j == 6)) || (i == 2 && j == 5)) 
+                {
+                    battle_zone[i][j] = 7;
+                }
+                printf("%d ", battle_zone[i][j]);
+            }
+            printf("\n"); 
+        }
+        break;
+
+    default:
+        break;
     }
-    
+
     return 0;
 }
